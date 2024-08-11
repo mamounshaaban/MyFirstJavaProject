45ee0c64c5129f1ef0a164d01d5b6728702362e8
mamounshaaban <saryshaban21@gmail.com>
2024-08-11 17:32:51 +0200

    Last updates  for tgis project

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
         // Press ⌃R or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
-
+sdklasdjljlkasdjkljasd
             // Press ⌃D to start debugging your code. We have set one breakpoint
             // for you, but you can always add more by pressing ⌘F8.
             System.out.println("i = " + i);
